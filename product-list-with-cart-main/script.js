a730aaa02bcd74abea49927da36b774986797d93
Елизавета Быкова <elizaveta.bykova@tuni.fi>
2024-07-23 20:03:52 -0200

    Button change created and Quantity now is updated on the button

--- product-list-with-cart-main/script.js
+++ product-list-with-cart-main/script.js
@@ -2,25 +2,75 @@ let cart = [];
 
 async function fetchProducts(){
     try{
-        const responce = await fetch('./data.json');
-        if(!responce.ok){
-            throw new Error(responce.status);
+        const response = await fetch('./data.json');
+        if(!response.ok){
+            throw new Error(response.status);
         }
-        const data = await responce.json();
+        const data = await response.json();
         console.log(data);
-        listOfProducts = document.getElementById('list-of-products');
-        data.map((item) => {
-            const listitem = createProductItem(item.category, item.image.desktop, item.name, item.price);
-            listOfProducts.append(listitem);
-        })
+        const listOfProducts = document.getElementById('list-of-products');
+        data.forEach((item) => {
+            const listItem = createProductItem(item.category, item.image.desktop, item.name, item.price);
+            listOfProducts.append(listItem);
+        });
     }catch(error){
         console.log(error);
     }
 }
 fetchProducts();
 
+function changeButton(product){
+    let button = document.getElementById(`change-button-${product.name}`);
+    button.innerHTML = '';
+
+    let removeItem = document.createElement('div');
+    removeItem.className = 'remove-icon';
+    const removeIcon = document.createElement('img');
+    removeIcon.src = 'assets/images/icon-decrement-quantity.svg';
+    removeIcon.alt ='remove';
+    removeItem.append(removeIcon);
+
+    removeItem.onclick = (e) => {
+        e.stopPropagation();
+        if(product.quantity > 1){
+            product.quantity--;
+            updateCart(product);
+            changeButton(product);
+        } else{
+            removeFromCart(product);
+        }
+    }
+
+    let quantityItem = document.createElement('div');
+    quantityItem.className = 'quantity-item';
+    let quantityValue = document.createElement('p');
+    quantityValue.innerText = product.quantity;
+    quantityItem.append(quantityValue);
+
+    quantityItem.onclick = (e) => {
+        e.stopPropagation();
+    }
+
+    let addItem = document.createElement('div');
+    addItem.className = 'add-icon';
+    const addIcon = document.createElement('img');
+    addIcon.src = './assets/images/icon-increment-quantity.svg';
+    addIcon.alt = 'add';
+    addItem.append(addIcon);
+
+    addItem.onclick = (e) => {
+        e.stopPropagation();
+        product.quantity++;
+        updateCart(product);
+        changeButton(product);
+    }
+
+    button.append(removeItem);
+    button.append(quantityItem);
+    button.append(addItem);
+}
+
 function createProductItem(category, imageSrc, name, price){
-    console.log(category + " " + imageSrc + " " + name + " " + price);
     const listItemContainer = document.createElement('div');
     listItemContainer.className = "list-item-container";
 
@@ -49,6 +99,7 @@ function createProductItem(category, imageSrc, name, price){
     priceItem.append(priceFull);
 
     let buttonInitial = document.createElement('p');
+    buttonInitial.id = `change-button-${name}`;
     const icon = document.createElement('img');
     icon.src = './assets/images/icon-add-to-cart.svg';
     icon.alt = 'Add to cart icon';
@@ -63,18 +114,15 @@ function createProductItem(category, imageSrc, name, price){
     listItemContainer.append(buttonInitial);
 
     buttonInitial.onclick = () => {
-        addToCart({name, price, imageSrc});
+        addToCart({ name, price, imageSrc });
     }
     return listItemContainer;
 }
 
-
-
 function renderCart() {
     const cartBox = document.getElementById('my-order');
     cartBox.innerHTML = '';
-    cart.forEach((product, index) => {
-        console.log(product.name + ' ' + product.price + ' ' + product.imageSrc);
+    cart.forEach((product) => {
         const cartItemContainer = document.createElement('div');
         cartItemContainer.className = "cart-item-container";
 
@@ -88,13 +136,12 @@ function renderCart() {
         priceBox.className = "cart-price-box";
 
         let amountOfProduct = document.createElement('p');
-        let count = 2;
-        amountOfProduct = count + 'x';
+        amountOfProduct.innerText = product.quantity + 'x';
 
         let priceItem = document.createElement('p');
         priceItem.innerText = '@ $' + product.price;
         let totalSumItem = document.createElement('p');
-        totalSumItem.innerText = '$' + count*product.price;
+        totalSumItem.innerText = '$' + (product.quantity * product.price);
 
         let cancelationButton = document.createElement('button');
         cancelationButton.className = 'cancel-button';
@@ -110,20 +157,61 @@ function renderCart() {
 
         cartItemContainer.append(cartNameItem);
         cartItemContainer.append(priceBox);
-        
-        cancelationButton.addEventListener('click', () => removeFromCart(index));
+
+        cancelationButton.addEventListener('click', () => removeFromCart(product));
 
         cartBox.append(cartItemContainer);
     });
 }
 
 function addToCart(product){
-    cart.push(product);
+    const cartItem = cart.find(item => item.name === product.name);
+    if(cartItem){
+        cartItem.quantity += 1;
+    } else{
+        product.quantity = 1;
+        cart.push(product);
+    }
+    console.log("Cart:", cart);
+    renderCart();
+    changeButton(product);
+}
+
+function updateCart(product) {
+    const cartItem = cart.find(item => item.name === product.name);
+    if (cartItem) {
+        cartItem.quantity = product.quantity;
+    }
+    console.log("Cart:", cart);
     renderCart();
 }
 
-function removeFromCart(index) {
-    console.log('removing product of index:' + index);
-    cart.splice(index,1);
+function makeButtonInitial(product){
+    let buttonInitial = document.getElementById(`change-button-${product.name}`);
+    buttonInitial.innerHTML = '';
+
+    const icon = document.createElement('img');
+    icon.src = './assets/images/icon-add-to-cart.svg';
+    icon.alt = 'Add to cart icon';
+    const text = document.createTextNode('Add to cart');
+    buttonInitial.appendChild(icon);
+    buttonInitial.appendChild(text);
+
+    buttonInitial.onclick = () => {
+        addToCart(product);
+    }
+}
+
+function removeFromCart(product) {
+    console.log('removing product:', product.name, product.quantity);
+    const cartItem = cart.find(item => item.name === product.name);
+    if(cartItem.quantity > 1){
+        cartItem.quantity -= 1;
+        updateCart(cartItem);
+        changeButton(cartItem);
+    } else{
+        cart.splice(cart.indexOf(cartItem), 1);
+        makeButtonInitial(product);
+    }
     renderCart();
 }
